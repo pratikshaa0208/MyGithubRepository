f803c47ef2b0e157778673ab33c2adccae1236d8
username <pratikshaonkar1999@gmail.com>
2023-04-15 19:16:18 +0530

    first commit

--- src/test/java/neoBasepackage/Base1.java
+++ src/test/java/neoBasepackage/Base1.java
@@ -4,20 +4,25 @@ import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Reporter;
+
+import neoStoxUtility.UtilityProperty;
 
 
 public class Base1
 {                                                           
-	 protected static  WebDriver  driver;        // declared globally because where we initialize the browser we have to use that driver olny to for whole execution otherwise if we use different diff. driver it will show null pointer . 
-   public void LaunchBrowser() throws InterruptedException, IOException 
-   {
-	   System.setProperty("webdriver.chrome.driver", "D:\\PratikshaVelocity\\chromedriver.exe");
-        driver=new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get(neoStoxUtility.UtilityProperty.readdatafromprorpertyfile("URL"));
-        Reporter.log("lanching browswer", true);
-       Thread.sleep(1000);
-   }
+	 protected static WebDriver driver;      // declared globally because where we initialize the browser we have to use that driver olny to for whole execution otherwise if we use different diff. driver it will show null pointer . 
+	   public void LaunchBrowser() throws InterruptedException, IOException 
+	   {
+		   System.setProperty("webdriver.chrome.driver", "D:\\PratikshaVelocity\\chromedriver.exe");
+	       ChromeOptions options=new ChromeOptions();
+	       options.addArguments("--remote-allow-origins=*");
+	        driver=new ChromeDriver(options);
+		    driver.manage().window().maximize();
+			driver.get(UtilityProperty.readdatafromprorpertyfile("URL"));
+	        Reporter.log("launching browswer", true);
+	       Thread.sleep(1000);
+	   }
 
 }
